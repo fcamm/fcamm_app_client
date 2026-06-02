5bb4189f077f47c48f4dc84077151e69c1697b96
anonems <93661372+anonems@users.noreply.github.com>
2026-06-02 10:59:01 +0200

    fixe build issue

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: 'donator/:id', renderMode: RenderMode.Client },
+  { path: 'admin/user/:id', renderMode: RenderMode.Client },
+  { path: '**', renderMode: RenderMode.Prerender }
 ];
